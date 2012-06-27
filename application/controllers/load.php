6de10bc53da8d8615d2d0654fd25c3805fd9b226
mihata <mihata@mihata-laptop.(none)>
2012-06-28 01:19:06 +0300

    load controller

---
+++ application/controllers/load.php
@@ -0,0 +1,14 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Load extends CI_Controller {
+
+    public function index()
+    {
+        $this->load->library('mongo_db');
+        $id = $this->mongo_db->insert('users', array('user1' => '123456'));
+        echo '<pre>';
+        var_dump($this->mongo_db->get('users'));
+        //echo $id;
+        //$this->load->view('welcome_message');
+    }
+}
