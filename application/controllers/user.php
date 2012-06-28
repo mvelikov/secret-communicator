32345e2490f32205b69ddc060635063a5c513b96
Misho <misho@misho-HP-Compaq-dc7700-Small-Form-Factor.(none)>
2012-06-28 15:40:08 +0300

    user

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -2,7 +2,26 @@
 
 class User extends MV_Controller {
 
-    public function index() {
-
+    public function index() 
+    {
+        $this->load->library('mongo_db');
+        $user = $this->mongo_db
+            ->get_where('users', array(
+                'user' => 'mvelikov',
+                'pass' => '7c4a8d09ca3762af61e59520943dc26494f8941b'
+            ));
+        echo '<pre>';
+        var_dump($user );
+    }
+    
+    public function insert() 
+    {
+        $this->load->library('mongo_db');
+        $id = $this->mongo_db
+                ->insert('users', array(
+                    'user' => 'mvelikov',
+                    'pass' => '7c4a8d09ca3762af61e59520943dc26494f8941b'
+                ));
+        echo $id;
     }
 }
