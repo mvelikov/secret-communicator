292dea8fef4d0249a6f0aff7cfedab3eee3fcef4
Misho <misho@misho-HP-Compaq-dc7700-Small-Form-Factor.(none)>
2012-07-01 16:00:44 +0300

    $_SERVER

--- application/core/MV_Controller.php
+++ application/core/MV_Controller.php
@@ -9,7 +9,7 @@ class MV_Controller extends CI_Controller {
         parent::__construct();
 
         $this->load->helper(array('mv_helper', 'url'));
-
+var_dump($_SERVER);exit();
         if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] != 'on')
         {
             redirect($this->router->class . '/error_https');
