b037b98114a73b1227ea27e2ac12db2572298695
Misho <misho@misho-HP-Compaq-dc7700-Small-Form-Factor.(none)>
2012-07-01 15:59:34 +0300

    fix base_url https

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://velikov-chat.phpfogapp.com/';
+$config['base_url']	= 'https://velikov-chat.phpfogapp.com/';
 
 /*
 |--------------------------------------------------------------------------
