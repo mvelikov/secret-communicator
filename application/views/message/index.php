7485c2c80e44f2b41b1805e361b47f5c7f1ab835
Mihail Velikov <mihailvelik@gmail.com>
2012-07-16 17:10:40 +0300

    defying list variable

--- application/views/message/index.php
+++ application/views/message/index.php
@@ -2,7 +2,9 @@
 header('Cache-Control: no-cache, must-revalidate');
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 header('Content-type: application/json');
-
+if (empty($list) OR !is_array($list)) {
+	$list = array();
+}
 echo json_encode(array(
     'code' => $code,
     'message' => $message,
