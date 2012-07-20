73d2e6fcd506fba8a1654930211a0d68ef5d9aac
Mihail Velikov <mihailvelik@gmail.com>
2012-07-20 19:04:10 +0300

    setting view for file uploaded cntrlr

--- application/views/file/error.php
+++ application/views/file/error.php
@@ -2,9 +2,9 @@
 header('Cache-Control: no-cache, must-revalidate');
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 header('Content-type: application/json');
-HTTPStatus(500);
+HTTPStatus(400);
 echo json_encode(array(
-	'code' => 500,
+	'code' => 400,
 	'message' => $error,
 	'success' => FALSE,
 	'failed' => TRUE,
