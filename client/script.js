f22568deb5eefb391cff3c9148b18d7de844a4cb
mihata <mihailvelik@gmail.com>
2012-07-30 22:18:46 +0300

    client set chosen channel msg

--- client/script.js
+++ client/script.js
@@ -174,7 +174,7 @@ $(document).ready(function() {
         e.preventDefault();
         $("#overlay").show();
         userObj.channel = $(this).attr('data-channel-id');
-        $("#channel-box").html($(this).html());
+        $("#channel-box").html('Channel: ' + $(this).html());
         uploader.setData({
             'pass' : userObj.pass,
             'channel' : userObj.channel
